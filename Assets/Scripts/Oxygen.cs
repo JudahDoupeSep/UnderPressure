b1960b4a312f9f3357bdc97d2c3826dd51264c8b
jjdoupe <jjdoupe@sep.com>
2023-03-19 12:58:53 -0400

    stuff

--- Assets/Scripts/Oxygen.cs
+++ Assets/Scripts/Oxygen.cs
@@ -1,29 +1,40 @@
 using System.Collections;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class Oxygen : MonoBehaviour
 {
     public VolumeProfile Profile;
     public float MaxOxygen = 60;
     public AnimationCurve VignetteCurve;
+    public AudioSource HeartBeat;
+    public AnimationCurve HeartRate;
+    public AnimationCurve HeartVolume;
+    public AudioSource Breath;
+    public AnimationCurve BreathVolume;
+    public float DeathTime = 2;
 
-    private float _oxygen = 0;
-    private float death => 1 - (_oxygen / MaxOxygen);
-    
-    void Start()
+    private float _oxygen;
+    private bool _dead;
+    private float death => 1 - _oxygen / MaxOxygen;
+
+    private void Start()
     {
         StartCoroutine(Respawn());
     }
 
-    void Update()
+    private void Update()
     {
+        if (_dead)
+            return;
+            
         _oxygen -= Time.deltaTime;
-        if(_oxygen <= 0)
+        if (_oxygen <= 0)
         {
+            _dead = true;
             StartCoroutine(Die());
         }
 
@@ -32,21 +43,54 @@ public class Oxygen : MonoBehaviour
 
     public void UpdateEffects()
     {
-        if (Profile.TryGet<Vignette>(out var vignette))
-        {
+        if (Profile.TryGet<Vignette>(out var vignette)) 
             vignette.intensity.value = VignetteCurve.Evaluate(death);
-        }
+        
+        HeartBeat.pitch = HeartRate.Evaluate(death);
+        HeartBeat.volume = HeartVolume.Evaluate(death);
+        Breath.volume = BreathVolume.Evaluate(death);
     }
 
     public IEnumerator Respawn()
     {
+        _dead = false;
         _oxygen = MaxOxygen;
-        yield return new WaitForSeconds(1);
+        
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+
+            if (Profile.TryGet<Exposure>(out var exposure))
+                exposure.compensation.value = (1 - t) * -5f;
+
+            yield return new WaitForEndOfFrame();
+        }
+        
+        
+        //TODO: start stuff
     }
-    
+
     public IEnumerator Die()
     {
-        yield return new WaitForSeconds(2);
+        var movement = FindObjectOfType<PlayerMovement>();
+        var oldSpeed = movement.moveSpeed;
+        
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / DeathTime;
+            movement.moveSpeed = (1 - t) * oldSpeed;
+
+            if (Profile.TryGet<Exposure>(out var exposure))
+                exposure.compensation.value = t * -5f;
+
+            yield return new WaitForEndOfFrame();
+        }
+        
+        
+        //TODO: death stuff
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         yield return Respawn();
     }
